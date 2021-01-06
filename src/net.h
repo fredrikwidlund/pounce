c268e128eaafbd69df868a633d7afe45cff02fbd
Fredrik Widlund <fredrik.widlund@gmail.com>
2021-01-06 22:45:25 +0000

    add missing define

--- src/net.h
+++ src/net.h
@@ -5,6 +5,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#define NET_CLIENT_DEFAULT (NET_FLAG_NONBLOCK | NET_FLAG_NODELAY)
+
 int net_client(struct addrinfo *, int);
 
 #endif /* NET_H_INCLUDED */
